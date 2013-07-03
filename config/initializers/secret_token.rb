7b2875dd192ab72db1c85b9367244945f39b23d1
Jordan Smith <jordan@jsmith.me>
2013-07-03 03:14:58 -0400

    Configured User signup/sign in using Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamble::Application.config.secret_token = '12b1a17ce1390c356a50f8d9238bb18f581e53297443887c350fa99f6fbe48dd8016f3fa218bf1dd0dcbefc2ba7fe5a3e241258b81ff537a8a370d3e824d37aa'
