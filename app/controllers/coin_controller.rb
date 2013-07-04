a4baf480321a44ef6030b07f45c8318f822e9e3d
Jordan Smith <jordan@jsmith.me>
2013-07-04 03:53:22 -0400

    Fix Coin Flip, Add Styling

--- app/controllers/coin_controller.rb
+++ app/controllers/coin_controller.rb
@@ -3,22 +3,24 @@ class CoinController < ApplicationController
 	before_filter :check, only: [:flip]
 
 	def index
+		@coin = "home"
 	end
 
   	def flip
   		ans = flipper()
 	  	token = current_user.tokens
 	  	bet = params[:bet].to_i
-
+	  	
 	  	if ans == params[:setting]
 	  		token = token + bet
 	  		current_user.update_attribute(:tokens, token)
-	  		flash[:notice] = "You won #{bet} tokens!"
+	  		flash.now[:won] = "You won #{bet} tokens!"
 	  	else
 	  		token = token - bet
 	  		current_user.update_attribute(:tokens, token)
-	  		flash[:notice] = "You lost #{bet} tokens"
+	  		flash.now[:lost] = "You lost #{bet} tokens"
 	  	end
+	  	@coin = ans
 	  	render 'index'
   	end
 
@@ -26,11 +28,12 @@ class CoinController < ApplicationController
   	private
   		def check
   			if params[:setting].nil?
-  				flash[:error] = "You must choose heads or tails"
+  				flash.now[:error] = "You must choose heads or tails"
+  				render 'index'
+  			elsif params[:bet].empty?
+  				flash.now[:error] = "You must enter a bet amount"
+  				render 'index'
   			end
-  			if params[:bet].empty?
-  				flash[:error] = "You must enter a bet amount"
-  			end
-  			render 'index'
+
   		end
 end
